1499585e9505c25d07026f8831b419ada05aabf0
arbunza <abubakarruwa60@gmail.com>
2024-07-22 22:53:02 +0100

    Initial commit for calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function clearDisplay(){
+    document.getElementById('display').innerText = '0'
+}
+
+function appendToDisplay(value){
+    const display = document.getElementById('display')
+    if(display.innerText === '0'){
+        display.innerText = value;
+    }else{
+        display.innerText += value;
+    }
+}
+
+function calcResult(){
+    const display = document.getElementById('display');
+    try{
+        const result = eval(display.innerText);
+        display.innerText = result;
+
+    }catch (error){
+        display.innerText = 'Error'
+    }
+}
